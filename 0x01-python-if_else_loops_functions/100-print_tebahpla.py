64b3ad58458a2c5ef9909e41080ae06b73727a1c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-08 11:08:15 -0700

    Adding 100-print_tebahpla.py file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,6 +3,7 @@
 a, z, i = 'a', 'z', 1
 
 while ord(z) >= ord(a):
-    print(z if i % 2 != 0 else chr(ord(z) - 32), end='')
+    print('{}'.format(z) if i % 2 != 0 else '{}'.format(chr(ord(z) - 32)),
+          end='')
     z = chr(ord(z) - 1)
     i += 1
