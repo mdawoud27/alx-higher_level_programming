c1dff349ca64c437ea946479fa6b5a6a0a73414b
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-08 04:01:25 -0700

    Adding 5-print_comb2.py file

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     print('{:d}'.format(i) if i > 10 else '0{:d}'.format(i), end='')
-    print('' if i == 99 else ', ', end='')
+    print('\n' if i == 99 else ', ', end='')
