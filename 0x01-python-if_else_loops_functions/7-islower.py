789aa470aca0e67f629369fd7d2ad7a9f4089411
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-08 09:36:19 -0700

    update the file

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-
-   if ord('a') <= ord(c) <= ord('z'):
+    if ord('a') <= ord(c) and ord(c) <= ord('z'):
         return True
-    return False
+    else:
+        return False
