9ea8e6b448a38d652db432d02033046320d78d8e
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-29 09:16:56 -0700

    Adding 0-square.py file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""This is a define for the square class."""
+
+
 class Square:
-    """This is an empty class that defines a square"""
+    """This is an empty class that defines a square."""
     pass
