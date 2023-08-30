e68630c02f685a2fbe6f0f3989f19a1b3e25e90b
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-29 20:25:59 -0700

    Adding 5-square file

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,3 +38,13 @@ class Square:
             Square area.
         """
         return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square."""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print('#', end='')
+            print()
