4b9bb01b1e3abb3de9508c75acc8fedafbf7a512
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-29 20:18:38 -0700

    Adding 4-square file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""This is a define for the square class."""
+
+
+class Square:
+    """This is an empty class that defines a square."""
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size value to set.
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Return:
+            Square area.
+        """
+        return self.__size * self.__size
