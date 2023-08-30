a5c7e1a29a1040e4456f335f67af8d73efdc0b2c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-08-29 22:26:24 -0700

    Adding 6-square file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,8 +12,8 @@ class Square:
             size (int): The size of the square.
             position (int, int): The coordinates of the square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
