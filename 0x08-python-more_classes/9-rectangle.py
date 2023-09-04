467399ba96c19c6f646e89e511cd99547112ea74
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-04 21:00:47 +0000

    Adding the file

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -69,8 +69,12 @@ class Rectangle:
         """Return a string representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join(
-                [str(self.print_symbol) * self.__width] * self.__height)
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         """__repr__ magic function"""
