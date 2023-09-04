f075be695b5e9cc8518f14ae8ea523819686b54f
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-04 19:50:04 +0000

    Adding the file

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -85,3 +85,23 @@ class Rectangle:
         """__del__ magic func"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Function that compares between rectangles.
+
+        Args:
+            rect_1: Instance of Rectangle.
+            rect_2: Instance of Rectangle.
+
+        Return:
+            The biggest rectangle based on the area.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
