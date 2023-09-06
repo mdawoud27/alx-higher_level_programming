9c74eda0ad5ed63619be38baa05e7241d7dc3d87
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-06 17:02:15 +0000

    Adding tests/6-max_integer_test.py file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,19 +7,19 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """class test for the max_integer func"""
     def test_empty_list(self):
-        self.assertEqual(max_integer.max_integer([]), None)
+        self.assertEqual(max_integer([]), None)
 
     def test_max_first(self):
-        self.assertEqual(max_integer.max_integer([9, 2, 0, 7]), 9)
+        self.assertEqual(max_integer([9, 2, 0, 7]), 9)
 
     def test_max_end(self):
-        self.assertEqual(max_integer.max_integer([9, 0, -3, 11]), 11)
+        self.assertEqual(max_integer([9, 0, -3, 11]), 11)
 
     def test_max_mid(self):
-        self.assertEqual(max_integer.max_integer([8, 0, 12, 10, -54]), 12)
+        self.assertEqual(max_integer([8, 0, 12, 10, -54]), 12)
 
     def test_max_minus(self):
-        self.assertEqual(max_integer.max_integer([-2, -12, 0, -5]), 0)
+        self.assertEqual(max_integer([-2, -12, 0, -5]), 0)
 
 
 if __name__ == '__main__':
