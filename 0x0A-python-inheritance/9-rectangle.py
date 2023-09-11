77f5d3f45405afc827ebd0a6aeb948d05963f200
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-11 16:43:24 +0000

    Adding 9-rectangle.py file

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,19 +2,7 @@
 """Module doc string"""
 
 
-class BaseGeometry:
-    """BaseGeometry class doc string"""
-
-    def area(self):
-        """Function doc string"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Function doc string"""
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BasGeometry
 
 
 class Rectangle(BaseGeometry):
