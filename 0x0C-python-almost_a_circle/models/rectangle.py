73bae7cece062484727e5d2fdf34465034a550fc
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-15 09:15:12 +0000

    Adding the Adding task 4 file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Return the area of rectangle."""
+        return self.__height * self.__width
