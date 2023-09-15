e35d759072bf88f03e0ebbf59d1d8440edfd2080
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-15 09:15:12 +0000

    Adding __str__()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,3 +102,7 @@ class Rectangle(Base):
             for w in range(self.__width):
                 print('#', end='')
             print()
+
+    def __str__(self):
+        """Prints in specific formate."""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
