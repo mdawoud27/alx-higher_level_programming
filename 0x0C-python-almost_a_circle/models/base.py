b411a4e7ca426ebd58a98080e723e01a70e38044
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-16 08:09:25 +0000

    Adding the file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Base class"""
+import json
 
 
 class Base:
     """Base class doc string"""
     __nb_objects = 0
-
     def __init__(self, id=None):
         """Class constractor.
 
@@ -16,3 +16,21 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Function that returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Function that writes the JSON string representation of list_objs to a file"""
+        file_name = cls.__name__ + '.json'
+        with open(file_name, 'w') as fn:
+            if list_objs is None:
+                fn.write('[]')
+            else:
+                obj_dicts = [obj.to_dictionary() for obj in list_objs]
+                fn.write(Base.to_json_string(obj_dicts))
