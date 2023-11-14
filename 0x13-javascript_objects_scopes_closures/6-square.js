b64660a99261400e2c0ea22c43c14b9f9f6cf678
dawoud <mhmaddawoud20@gmail.com>
2023-11-14 15:48:00 +0200

    Adding 6-square.js file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ class Square extends baseSquare {
 
   charPrint (c) {
     let char = '';
-    c === undefined ? char = 'X' : char = c;
+    c == undefined ? char = 'X' : char = c;
 
     for (let i = 0; i < this.size; i++) {
       let squRow = '';
