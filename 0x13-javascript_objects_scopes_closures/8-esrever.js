33e0ab03663dd92d912fbc0bd1a935b50843d04e
dawoud <mhmaddawoud20@gmail.com>
2023-11-14 18:19:04 +0200

    Adding the remaining files

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,6 +4,7 @@ exports.esrever = function (list) {
   const reversedList = [];
   for (let i = list.length - 1, k = 0; i >= 0; i--, k++) {
     reversedList[k] = list[i];
+    // Or `reversedList.push(list[i]);`
   }
   return reversedList;
 };
