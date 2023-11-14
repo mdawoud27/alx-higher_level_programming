6a25ed1056ae4b2782f18a8597548e351fb925ad
dawoud <mhmaddawoud20@gmail.com>
2023-11-14 15:46:21 +0200

    Adding 6-square.js file

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const baseSquare = require('./5-square.js');
+
+class Square extends baseSquare {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    let char = '';
+    c === undefined ? char = 'X' : char = c;
+
+    for (let i = 0; i < this.size; i++) {
+      let squRow = '';
+      for (let j = 0; j < this.size; j++) {
+        squRow += char;
+      }
+      console.log(squRow);
+    }
+  }
+}
+
+module.exports = Square;
