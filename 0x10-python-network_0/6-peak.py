cacab28e788dbc85ee5c3d9312fc7ac4ff07dd55
dawoud <mhmaddawoud20@gmail.com>
2023-11-30 16:48:29 +0200

    Adding 6-peak.py file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,18 +4,8 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
-    if not list_of_integers:
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
+    else:
         return None
-
-    low = 0
-    high = len(list_of_integers) - 1
-
-    while low < high:
-        mid = (low + high) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
-        else:
-            low = mid + 1
-
-    return list_of_integers[low]
