2573196fc66be69881f6f71f166aadac92b2d897
dawoud <mhmaddawoud20@gmail.com>
2023-11-30 16:17:51 +0200

    Adding 3-methods.sh file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX OPTIONS -i "$1" | grep -i "Allow" | awk -F ': ' '{print $2}'
+curl -sI "$1" | grep Allow | cut -d' ' -f2-
