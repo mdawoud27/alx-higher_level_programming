1c86a929d6ada57a14df0e1fe22a94f3804ca571
dawoud <mhmaddawoud20@gmail.com>
2023-11-30 20:41:37 +0200

    Adding 102-catch_me.sh file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Catch me if you can!
-curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool"
+curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" -d "user_id=98"
