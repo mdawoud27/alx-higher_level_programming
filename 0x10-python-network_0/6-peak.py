24c3d0dc3b3b63652f4bd7b595ea212839dd5236
dawoud <mhmaddawoud20@gmail.com>
2023-11-30 16:43:55 +0200

    Adding 6-peak.py file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!usr/bin/python3
 """Module for finding a peak in a list of unsorted integers."""
+
+
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if not list_of_integers:
