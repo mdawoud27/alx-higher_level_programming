c1e98f3e22fea2bc15c6141f8b615f71e2be32e4
dawoud <mhmaddawoud20@gmail.com>
2023-12-01 13:12:19 +0200

    Adding 6-post_email.py file

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     import sys
 
     url = sys.argv[1]
-    email = sys.argv[2].encode('utf-8')
+    email = sys.argv[2]
 
     payload = {'email': email}
     response = requests.post(url, data=payload)
-    print(f"Your email is: {response.text}")
+    print(response.text)
