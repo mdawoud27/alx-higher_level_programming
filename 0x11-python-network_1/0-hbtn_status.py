69d5077074b9fd4d7e883fbc5a3c2b7b6395679d
dawoud <mhmaddawoud20@gmail.com>
2023-12-01 11:09:46 +0200

    Adding 0-hbtn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import urllib.request as url_req
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+    import urllib.request as url_req
 
-with url_req.urlopen(url) as response:
-    content = response.read()
+    url = 'https://alx-intranet.hbtn.io/status'
 
-print("Body response:")
-print(f"    - type: {type(content)}")
-print(f"    - content: {content}")
-print(f"    - utf8 content: {content.decode('utf-8')}")
+    with url_req.urlopen(url) as response:
+        content = response.read()
+
+    print("Body response:")
+    print(f"    - type: {type(content)}")
+    print(f"    - content: {content}")
+    print(f"    - utf8 content: {content.decode('utf-8')}")
