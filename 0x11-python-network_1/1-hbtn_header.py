faa10395fe392f740bca03a8e03714051149a7ab
dawoud <mhmaddawoud20@gmail.com>
2023-12-01 11:16:08 +0200

    Adding 1-hbtn_header.py file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import urllib.request as url_req
-import sys
 
-url = sys.argv[1]
-with url_req.urlopen(url) as response:
-    header_value = response.getheader('X-Request-Id')
-    print(header_value)
+if __name__ == '__main__':
+    import urllib.request as url_req
+    import sys
+
+    url = sys.argv[1]
+    with url_req.urlopen(url) as response:
+        header_value = response.getheader('X-Request-Id')
+        print(header_value)
