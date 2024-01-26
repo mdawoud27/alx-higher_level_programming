8985a65b541393cb14c99dcdff0f9a9a0b124edf
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-01-26 18:10:38 +0200

    Adding 101-avg_temperatures.sql file

---
+++ 0x0D-SQL_introduction/temperatures.sql
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>X202F1TW57K0KXQZ</RequestId><HostId>FnDJpZinWfIZbP72iVGjp6KQQwrintRptP9o9MpiTDdH7kJHOQUHEghfgOp94/NVbkUNdPXz/d0=</HostId></Error>
