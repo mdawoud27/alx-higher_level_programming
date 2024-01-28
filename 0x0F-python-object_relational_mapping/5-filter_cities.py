beab89a9ef2a594abfee8bb47e5e9d18e6dd4263
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-01-28 18:26:53 +0200

    Adding 5-filter_cities.py file

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[1],
+        password=args[2],
+        database=args[3]
+    )
+    cursor = connection.cursor()
+    cursor.execute("""SELECT DISTINCT cities.name FROM cities
+                      JOIN states ON cities.state_id = states.id
+                      WHERE states.name = %s""", (args[4],))
+    query_rows = cursor.fetchall()
+    for i, row in enumerate(query_rows):
+        print(*row, end=', ' if i < len(query_rows) - 1 else '\n')
+
+    cursor.close()
+    connection.close()
