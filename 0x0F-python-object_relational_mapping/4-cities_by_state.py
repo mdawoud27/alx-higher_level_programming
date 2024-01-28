7e0ee2ea6aa04f6372b1d400d5795ca0ec6ce39c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-01-28 17:14:17 +0200

    Adding 4-cities_by_state.py file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     )
     cursor = connection.cursor()
     query = """SELECT cities.id, cities.name, states.name FROM cities
-               RIGHT JOIN states
+               INNER JOIN states
                ON cities.state_id = states.id
                ORDER BY cities.id ASC"""
     cursor.execute(query)
