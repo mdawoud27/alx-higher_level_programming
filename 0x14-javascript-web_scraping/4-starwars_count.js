2f6eb9c43ef76e8f351abb4052758ecc7c657922
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-04-04 17:26:53 +0200

    Adding the file

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,9 +3,8 @@
 const url = process.argv[2];
 const request = require('request');
 
-request.get('https://swapi-api.alx-tools.com/api/people/18', (err, res, body) => {
+request.get(url, (err, res, body) => {
   if (err) throw err;
-  const bodyObj = JSON.parse(body);
-  console.log(bodyObj.films.length);
-//   console.log(body.split('/people/18/'.length - 1));
+  const count = body.split('/people/18/').length - 1;
+  console.log(count);
 });
