e829e02b1a781b1e8527ea2910ef1839c4f0d804
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-04-04 15:35:46 +0200

    Adding script which read data from file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-filePath = process.argv[2];
+const filePath = process.argv[2];
 const fs = require('fs');
 
-fs.open(filePath, 'r', 'utf-8', (err, file) => {
+fs.open(filePath, 'r', (err, file) => {
   if (err) throw err;
   fs.readFile(filePath, (err, data) => {
     if (err) throw err;
