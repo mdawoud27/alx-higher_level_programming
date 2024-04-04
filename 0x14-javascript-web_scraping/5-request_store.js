bef9cbd9da919728cceaffd29719393e52fdfca2
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-04-04 19:10:39 +0200

    Script that gets the content of webPage in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,10 +6,8 @@ const request = require('request');
 const fs = require('fs');
 
 request.get(url, (err, req, body) => {
-  fs.open(filePath, 'w', (err) => {
+  if (err) throw err;
+  fs.writeFile(filePath, body, (err) => {
     if (err) throw err;
-    fs.writeFile(filePath, body, (err) => {
-      if (err) throw err;
-    });
   });
 });
