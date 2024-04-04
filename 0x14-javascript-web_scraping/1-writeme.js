40cbf046c30d9a662183785d5d2831e6417157b1
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-04-04 16:19:19 +0200

    Script that writes in the file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,11 +2,8 @@
 
 const filepath = process.argv[2];
 const str = process.argv[3];
-const fs = require("fs");
+const fs = require('fs');
 
-fs.open(filepath, 'w', (err) => {
-  if (err) throw err;
-  fs.writeFile(filepath, str, (err) => {
-    if (err) throw err;
-  });
+fs.writeFile(filepath, str, (err) => {
+  if (err) console.log(err);
 });
